818c49f29f9c82c533adeef37bc81071b00131a7
Nycto <James@RoundEights.com>
2019-03-20 08:40:05 -0700

    Use generics for type conversions

--- src/safegl/vectors.nim
+++ src/safegl/vectors.nim
@@ -2,49 +2,48 @@ import opengl, macros
 
 type GLvectorb2* = array[2, GLbyte]
 
-macro createConverter(inputType, outputType, glVectorRoot: untyped): untyped =
-    ## Creates conversion methods for interop between nim native types and OpenGL types
-    expectKind inputType, nnkIdent
-    expectKind outputType, nnkIdent
-    expectKind glVectorRoot, nnkIdent
+export GLmatrixi3, GLint, GLbyte, GLubyte
+export GLvectorf2, GLvectorf3, GLvectorf4, GLvectori2, GLvectori3, GLvectori4
+export GLmatrixi3, GLmatrixf3, GLmatrixd3, GLmatrixub4, GLmatrixi4, GLmatrixf4, GLmatrixd4
 
-    result = newStmtList()
-    for i in 2..4:
+type
+    Matrix*[Rows, Cols: static[int], T: SomeNumber] = array[Rows, array[Cols, T]] ## Matrix alias type
+    Vector*[Cols: static[int], T: SomeNumber] = array[Cols, T] ## Vector alias type
 
-        let vectorType = ident(glVectorRoot.strVal & $i)
+proc `[]`*[Rows, Cols: static[int], T](self: Matrix[Rows, Cols, T], x, y: int): T {. inline .} =
+    ## Single bracket access to an x/y coordinate
+    result = self[x][y]
 
-        # Add an export statement
-        result.add(nnkExportStmt.newTree(vectorType, outputType))
+template convertVector(source: typed, cols: int, assign: untyped, asType: typedesc): untyped =
+    ## Converts elements in the given vector to a vector of a new type
+    for i in 0..cols - 1:
+        assign[i] = asType(source[i])
 
-        let elements = nnkBracket.newTree()
+converter toGlintVector*[Cols: static[int]](source: Vector[Cols, SomeInteger]): Vector[Cols, GLint] {. inline .} =
+    ## Converts a matrix of ints to a matrix of GLints
+    convertVector(source, Cols, result, GLint)
 
-        # Add the convert method
-        result.add(nnkConverterDef.newTree(
-            postfix(ident("to" & vectorType.strVal), "*"),
-            newEmptyNode(),
-            newEmptyNode(),
-            nnkFormalParams.newTree(
-                vectorType,
-                nnkIdentDefs.newTree(
-                    ident("input"),
-                    nnkBracketExpr.newTree(ident("array"), newLit(i), inputType),
-                    newEmptyNode()
-                )
-            ),
-            newEmptyNode(),
-            newEmptyNode(),
-            newStmtList(
-                newCommentStmtNode("Convert an array of " & inputType.strVal & " to a vector of " & outputType.strVal),
-                elements
-            )
-        ))
+converter toGlfloatVector*[Cols: static[int]](source: Vector[Cols, SomeNumber]): Vector[Cols, GLfloat] {. inline .} =
+    ## Converts a matrix of ints to a matrix of GLfloats
+    convertVector(source, Cols, result, GLfloat)
 
-        for elem in 0..(i-1):
-            elements.add(newDotExpr(nnkBracketExpr.newTree(ident("input"), newLit(elem)), outputType))
+template convertMatrix(source: typed, rows, cols: int, asType: typedesc): untyped =
+    ## Converts between two matrix types
+    for y in 0..rows - 1:
+        let row = source[y]
+        convertVector(row, Cols, result[y], asType)
 
-createConverter(SomeFloat, GLfloat, GLvectorf)
-createConverter(SomeInteger, GLfloat, GLvectorf)
-createConverter(SomeInteger, GLint, GLvectori)
-createConverter(SomeInteger, GLbyte, GLvectorb)
-createConverter(SomeInteger, GLubyte, GLvectorub)
+converter toGlintMatrix*[Rows, Cols: static[int]](
+    source: Matrix[Rows, Cols, SomeInteger]
+): Matrix[Rows, Cols, GLint] {. inline .} =
+    ## Converts a matrix of ints to a matrix of GLints
+    convertMatrix(source, Rows, Cols, GLint)
+
+converter toGlfloatMatrix*[Rows, Cols: static[int]](
+    source: Matrix[Rows, Cols, SomeNumber]
+): Matrix[Rows, Cols, GLfloat] {. inline .} =
+    ## Converts a matrix of ints to a matrix of GLfloats
+    convertMatrix(source, Rows, Cols, GLfloat)
+
+
 
